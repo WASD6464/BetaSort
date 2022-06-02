4a17e28a1b140eb9204987b9c3072e5701263587
David <wasd64@MacBook-Pro-David.local>
2022-06-02 14:22:33 +0300

    BetaSort complete

--- database/autobd.py
+++ database/autobd.py
@@ -5,5 +5,4 @@ while True:
     if os.path.exists("your_video.txt"):
         os.chdir("/Users/wasd64/Desktop/Code/VVIT/DeepSort/database/")
         os.system("python bd_append.py")
-        os.chdir("/Users/wasd64/Desktop/Code/VVIT/DeepSort/static/Complete/")
         break
